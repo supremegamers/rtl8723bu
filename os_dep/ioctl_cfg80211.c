8fda99cf12469999987c6cb82fe86be3f8100cf5
hmtheboy154 <buingoc67@gmail.com>
2022-08-11 23:33:54 +0700

    cfg80211 : Comply with Google's backport
    New backport commits https://github.com/hmtheboy154/Darkmatter-kernel/commit/6abf3c5ef229a88ead08651c5dd54bee0293a3a2 & https://github.com/hmtheboy154/Darkmatter-kernel/commit/d975927513c72b595b6762adb7d3c5962cc1a8d9 causing the source unable to compile, this commit will fix it.

--- os_dep/ioctl_cfg80211.c
+++ os_dep/ioctl_cfg80211.c
@@ -747,8 +747,8 @@ check_bss:
 
 		DBG_871X(FUNC_ADPT_FMT" call cfg80211_roamed\n", FUNC_ADPT_ARG(padapter));
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,12,0))
-		roam_info.channel = notify_channel;
-		roam_info.bssid = cur_network->network.MacAddress;
+		roam_info.links[0].channel = notify_channel;
+		roam_info.links[0].bssid = cur_network->network.MacAddress;
 		roam_info.req_ie =
 			pmlmepriv->assoc_req+sizeof(struct rtw_ieee80211_hdr_3addr)+2;
 		roam_info.req_ie_len =
@@ -3969,7 +3969,8 @@ static int cfg80211_rtw_change_beacon(struct wiphy *wiphy, struct net_device *nd
 	return ret;
 }
 
-static int cfg80211_rtw_stop_ap(struct wiphy *wiphy, struct net_device *ndev)
+static int cfg80211_rtw_stop_ap(struct wiphy *wiphy, struct net_device *ndev,
+				unsigned int link_id)
 {
 	DBG_871X(FUNC_NDEV_FMT"\n", FUNC_NDEV_ARG(ndev));
 	return 0;
